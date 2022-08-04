baae194e0806082ad87b3cdc86332d329a061b22
Philipp Glaum <p.glaum@tu-berlin.de>
2022-08-04 20:59:43 +0200

    update paper and cost_bar plot

--- scripts/plot_bars.py
+++ scripts/plot_bars.py
@@ -153,7 +153,7 @@ def plot_historical_curtailment(ax, networks):
 
 
 def plot_cost(ax, networks):
-    costs = get_costs(networks[0]) - get_costs(networks[1])
+    costs = get_costs(networks[1]) - get_costs(networks[0])
     costs /= 1e9
     costs.plot.barh(stacked=True, ax=ax, zorder=4, legend="reverse")
     ax.grid(linestyle="--", linewidth=0.5, alpha=0.5, zorder=2)
